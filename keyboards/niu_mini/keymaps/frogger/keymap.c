141b7df19108549623850f947ec8ad2b5ab63a7b
Dave Brown <davebrown.dev@gmail.com>
2018-09-03 20:48:44 -0400

    more tuning

--- keyboards/niu_mini/keymaps/frogger/keymap.c
+++ keyboards/niu_mini/keymaps/frogger/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | GUI |  Ctrl | ALT  |NUMPAD|LOWER |    Space    |RAISE |  /   |  {   |   }  |   |  |
+	 * | GUI |  Ctrl | ALT  |NUMPAD|LOWER |    Space    |RAISE |   =  |  {   |   }  |   |  |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_QWERTY] = LAYOUT_planck_mit(
 		KC_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,      KC_BSPC,
 		LT(_LOWER, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT,
-		KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUMPAD), LOWER,KC_SPC,RAISE, KC_UP ,  KC_LCBR, KC_RCBR,   KC_PIPE
+		KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUMPAD), LOWER,KC_SPC,RAISE, KC_EQL ,  KC_LCBR, KC_RCBR,   KC_PIPE
   ),
 
    /* LOWER
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* ,-----------------------------------------------------------------------------------.
 	* |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   Up |   (  |   )  | Del  |
 	* |------+------+------+------+------+-------------+------+------+------+------+------|
-	* |      |      |      |      |      |      | Home | Left | Down |Right | End  |  |   |
+	* |      |      |      |      |      |      | Home | Left | Down |Right | End  |     |
 	* |------+------+------+------+------+------|------+------+------+------+------+------|
 	* |      |      |      |      |      |      |      |wordLe|      |wordRi|      |      |
 	* |------+------+------+------+------+------+------+------+------+------+------+------| 
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*/
 	[_LOWER] = LAYOUT_planck_mit(
 		KC_GRAVE, KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,   KC_AMPR,         KC_UP,    KC_LPRN,         KC_RPRN,  KC_DEL,
-		_______, _______, _______,  _______,  _______,  _______,  KC_HOME,   KC_LEFT,         KC_DOWN,  KC_RIGHT,        KC_END,   KC_PIPE,
+		_______, _______, _______,  _______,  _______,  _______,  KC_HOME,   KC_LEFT,         KC_DOWN,  KC_RIGHT,        KC_END,   _______,
 		_______, _______, _______,  _______,  _______,  _______,  _______,   LALT(KC_LEFT),   _______,  LALT(KC_RIGHT),  _______,  _______,
 		_______, _______, _______,  _______,  _______,      _______,         _______,         _______,  _______,         _______,  _______
 	),
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_RAISE] = LAYOUT_planck_mit(
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
+		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_BSLASH,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,    _______,    KC_MINS, KC_EQL,  _______,
 		_______, _______, _______, _______, _______,     _______,      _______,    _______,    _______, _______, _______
 	),
