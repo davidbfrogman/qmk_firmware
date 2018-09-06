3fc6b1d249acd6f429bc1e0e484a2566f9fa459c
Dave Brown <davebrown.dev@gmail.com>
2018-09-05 21:49:56 -0400

    Adding slash in.

--- keyboards/niu_mini/keymaps/frogger/keymap.c
+++ keyboards/niu_mini/keymaps/frogger/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |------+------+------+------+------+------|------+------+------+------+------+------|
 	* |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |   +  |   [  |  ]   |  |   |
 	* |------+------+------+------+------+------+------+------+------+------+------+------|
-	* |      |      |      |      |      |             |      |      |      |      |      |
+	* |      |      |      |      |      |             |      |      |      |      |   /  |
 	* `-----------------------------------------------------------------------------------'
 	*/
 
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
 		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS,    KC_EQL,     KC_LCBR, KC_RCBR, KC_BSLASH,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,  KC_PIPE,
-		_______, _______, _______, _______, _______,     _______,      _______,    _______,    _______, _______, _______
+		_______, _______, _______, _______, _______,     _______,      _______,    _______,    _______, _______, KC_SLSH
 	),
 
   	/* NUMPAD 
