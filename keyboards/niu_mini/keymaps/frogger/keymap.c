cc092d3f26007cf272c18ffe8e80cec967f2ee70
Dave Brown <davebrown.dev@gmail.com>
2018-09-05 21:31:31 -0400

    more tweaks

--- keyboards/niu_mini/keymaps/frogger/keymap.c
+++ keyboards/niu_mini/keymaps/frogger/keymap.c
@@ -27,28 +27,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | GUI |  Ctrl | ALT  |NUMPAD|LOWER |    Space    |RAISE |   =  |  {   |   }  |   |  |
+	 * | GUI |  Ctrl | ALT  |NUMPAD|LOWER |    Space    |RAISE |   {  |  (   |   )  |   }  |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_QWERTY] = LAYOUT_planck_mit(
 		KC_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,      KC_BSPC,
 		LT(_LOWER, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT,
-		KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUMPAD), LOWER,KC_SPC,RAISE, KC_EQL ,  KC_LCBR, KC_RCBR,   KC_PIPE
+		KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUMPAD), LOWER,KC_SPC,RAISE, KC_LCBR ,  KC_LPRN, KC_RPRN,   KC_RCBR
   ),
 
    /* LOWER
-    Think about creating a colummn for the underscore equal brackets semi, period 
-	that way you wouldn't have to think as much about where those keys are at.
 		
 	* ,-----------------------------------------------------------------------------------.
 	* |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   Up |   (  |   )  | Del  |
 	* |------+------+------+------+------+-------------+------+------+------+------+------|
-	* |      |      |      |      |      |      | Home | Left | Down |Right | End  |     |
+	* |      |      |      |      |      |      | Home | Left | Down |Right | End  |      |
 	* |------+------+------+------+------+------|------+------+------+------+------+------|
 	* |      |      |      |      |      |      |      |wordLe|      |wordRi|      |      |
 	* |------+------+------+------+------+------+------+------+------+------+------+------| 
-	* |      |      |      |      |      |             |      |      |      |       |     |
+	* |      |      |      |      |      |             |      |      |      |      |      |
 	* `-----------------------------------------------------------------------------------'
 	*/
 	[_LOWER] = LAYOUT_planck_mit(
@@ -62,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* ,-----------------------------------------------------------------------------------.
 	* |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
 	* |------+------+------+------+------+-------------+------+------+------+------+------|
-	* | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+	* | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   {  |   }  |  \   |
 	* |------+------+------+------+------+------|------+------+------+------+------+------|
-	* |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |   -  |  =   |      |
+	* |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |   +  |   [  |  ]   |  |   |
 	* |------+------+------+------+------+------+------+------+------+------+------+------|
 	* |      |      |      |      |      |             |      |      |      |      |      |
 	* `-----------------------------------------------------------------------------------'
@@ -72,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_RAISE] = LAYOUT_planck_mit(
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_BSLASH,
-		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,    _______,    KC_MINS, KC_EQL,  _______,
+		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS,    KC_EQL,     KC_LCBR, KC_RCBR, KC_BSLASH,
+		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,  KC_PIPE,
 		_______, _______, _______, _______, _______,     _______,      _______,    _______,    _______, _______, _______
 	),
 
@@ -89,10 +87,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `-----------------------------------------------------------------------------------'
 	 */ 
 	[_NUMPAD] = LAYOUT_planck_mit(
-		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+		_______, KC_1,    KC_2,          KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
 		_______, _______, LCTL(S(KC_S)), _______, _______, _______, _______, KC_4,    KC_5,    KC_6, _______, _______,
-		_______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3, _______, _______,
-		_______, _______, _______, _______, _______,     _______,   _______,    KC_0, _______, _______, _______
+		_______, _______, _______,       _______, _______, _______, _______, KC_1,    KC_2,    KC_3, _______, _______,
+		_______, _______, _______,       _______, _______,     _______,   _______,    KC_0, _______, _______, _______
   ),
 
     /* RGB control. Raise and Lower at the same time. 
@@ -101,9 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * |      |BLtogg|BLstep|      |      |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      |      |      |Prev  |Play  |Next  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |             |      |      |      |      | RESET|
+   * |      |      |      |      |      |             |      |      |VolDo |Mute  |Vol U |
    * `-----------------------------------------------------------------------------------'
    HUE Increase = HUI
    Saturatoin Increase = SAI
