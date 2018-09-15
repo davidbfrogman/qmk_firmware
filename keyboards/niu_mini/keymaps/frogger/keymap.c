9cb75a4c36411786b504f5fe1c2413296c102499
Dave Brown <davebrown.dev@gmail.com>
2018-09-14 20:27:08 -0400

    updates to my niu

--- keyboards/niu_mini/keymaps/frogger/keymap.c
+++ keyboards/niu_mini/keymaps/frogger/keymap.c
@@ -27,15 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | Ctrl |  GUI | ALT  |NUMPAD|LOWER |    Space    |RAISE |   {  |  (   |   )  |   }  |
+	 * | Ctrl |  GUI | ALT  |NUMPAD|LOWER |    Space    |RAISE |   (  |  {   |   }  |   )  |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_QWERTY] = LAYOUT_planck_mit(
 		KC_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,      KC_BSPC,
 		LT(_LOWER, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT,
-		KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUMPAD), LOWER,KC_SPC,RAISE,KC_LPRN ,  KC_LCBR, KC_RCBR,   KC_RPRN
+		KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUMPAD), LOWER,KC_SPC,RAISE,KC_LPRN,  KC_LCBR, KC_RCBR,   KC_RPRN
   ),
+
 
    /* LOWER
 		
@@ -56,6 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______,  _______,  _______,      _______,         _______,         _______,  _______,         _______,  _______
 	),
 
+
    /* RAISE
 	* ,-----------------------------------------------------------------------------------.
 	* |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
@@ -67,13 +69,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |      |      |      |      |      |             |      |      |      |      |      |
 	* `-----------------------------------------------------------------------------------'
 	*/
-
 	[_RAISE] = LAYOUT_planck_mit(
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
 		_______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS,    KC_EQL,     KC_LCBR, KC_RCBR, KC_BSLASH,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS,    KC_PLUS,    KC_LBRC, KC_RBRC,  KC_PIPE,
 		_______, _______, _______, _______, _______,     _______,      _______,    _______,    _______, _______, _______
 	),
+
+
 
   	/* NUMPAD 
 	 * ,-----------------------------------------------------------------------------------.
@@ -93,13 +96,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______,       _______, _______,     _______,   _______,    KC_0, _______, _______, _______
   ),
 
+
+
     /* RGB control. Raise and Lower at the same time. 
    * ,-----------------------------------------------------------------------------------.
    * | RESET|RGBtog|RGBmod|RGBhud|RGBhui|RGBsai|RGBsad|      |      |      |      |      |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * |      |BLtogg|BLstep|      |      |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |Prev  |Play  |Next  |
+   * |      |      |      |      |      |      |      |      |Prev  |Play  |Next  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |             |      |      |VolDo |Mute  |Vol U |
    * `-----------------------------------------------------------------------------------'
@@ -113,13 +118,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, _______, _______,  _______,  _______,      _______,        _______,   _______,   KC_VOLD,  KC_MUTE,  KC_VOLU
   )
 };
-
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
